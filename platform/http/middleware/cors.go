e47c954de3ff75f222991fcde75dcf486cfd42b3
kjuiop <copying_y@naver.com>
2026-03-28 15:40:43 +0900

    [#7] fix: 배포 단계에서 cors 적용될 수 있도록 변경

--- platform/http/middleware/cors.go
+++ platform/http/middleware/cors.go
@@ -14,7 +14,7 @@ func SetCorsPolicy() gin.HandlerFunc {
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           12 * time.Hour,
 	})
 }
